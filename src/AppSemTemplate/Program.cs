87a7a0399f7de26120ef835dbba9159f6642327c
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-07-28 20:36:23 -0300

    Criando o Crud com o scaffolding  e implementando taghelper para os botões do crud

--- src/AppSemTemplate/Program.cs
+++ src/AppSemTemplate/Program.cs
@@ -1,6 +1,15 @@
+using AppSemTemplate.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(o =>
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
